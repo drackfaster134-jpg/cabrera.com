dfd11d571e38c22fcf779e41ae4d5cd26a8b523f
drackfaster134-jpg <drackfaster134@gmail.com>
2025-12-02 18:42:52 -0500

    Initialize Firebase and add login functionality

--- auth.js
+++ auth.js
@@ -20,3 +20,47 @@ async function esAdmin(email) {
   const listaAdmins = doc.data().lista;
   return listaAdmins.includes(email);
 }
+// CONFIGURACIÓN DE FIREBASE
+const firebaseConfig = {
+  apiKey: "AIzaSyDxl87oXbrBWbX7yiBaPSMzMF6Wn_3pDwM",
+  authDomain: "cabrera134-347c2.firebaseapp.com",
+  projectId: "cabrera134-347c2",
+  storageBucket: "cabrera134-347c2.firebasestorage.app",
+  messagingSenderId: "133005322978",
+  appId: "1:133005322978:web:5957abcd38667ab92d4cbb",
+  measurementId: "G-GK84PTTJ5D"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+// FUNCIÓN: Verificar si es admin
+async function esAdmin(email) {
+  const doc = await db.collection("config").doc("admins").get();
+  const listaAdmins = doc.data().lista;
+  return listaAdmins.includes(email);
+}
+
+// FUNCIÓN: Login
+async function login() {
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("pass").value;
+
+  try {
+    await auth.signInWithEmailAndPassword(email, pass);
+
+    if (await esAdmin(email)) {
+      alert("Bienvenido ADMIN");
+      window.location.href = "categorias.html";
+    } else {
+      alert("Bienvenido VISITANTE");
+      window.location.href = "trabajos.html";
+    }
+
+  } catch (error) {
+    alert("Error: " + error.message);
+  }
+}
