ab9146f11b7499e5fa07cbf35f162e5f23aba00b
drackfaster134-jpg <drackfaster134@gmail.com>
2025-12-02 19:07:53 -0500

    Update roles.js

--- roles.js
+++ roles.js
@@ -1,3 +1,4 @@
+// Verificar si hay usuario logueado
 firebase.auth().onAuthStateChanged(async (user) => {
     if (!user) {
         // Si no está logueado → mandarlo al login
@@ -6,10 +7,10 @@ firebase.auth().onAuthStateChanged(async (user) => {
     }
 
     // Consultar si es admin
-    const doc = await db.collection("config").doc("admins").get();
+    const doc = await firebase.firestore().collection("config").doc("admins").get();
     const listaAdmins = doc.data().lista;
     const admin = listaAdmins.includes(user.email);
 
-    // Guardar rol para otras páginas
+    // Guardar rol
     localStorage.setItem("rol", admin ? "admin" : "visitante");
 });
