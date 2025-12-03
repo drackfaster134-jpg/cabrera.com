13a3945821f66ea2c93c03e23cf6925bdab56b25
drackfaster134-jpg <drackfaster134@gmail.com>
2025-12-02 20:17:58 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDxl87oXbrBWbX7yiBaPSMzMF6Wn_3pDwM",
   authDomain: "cabrera134-347c2.firebaseapp.com",
   projectId: "cabrera134-347c2",
-  storageBucket: "cabrera134-347c2.firebasestorage.app",
+  storageBucket: "cabrera134-347c2.appspot.com",
   messagingSenderId: "133005322978",
   appId: "1:133005322978:web:5957abcd38667ab92d4cbb",
   measurementId: "G-GK84PTTJ5D"
@@ -11,18 +11,18 @@ const firebaseConfig = {
 
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
-
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 // Verificar si es admin
 async function esAdmin(email) {
   const doc = await db.collection("config").doc("admins").get();
-  const listaAdmins = doc.data().lista;
+  const listaAdmins = doc.data().lista || [];
   return listaAdmins.includes(email);
 }
 
-// FUNCIÓN LOGIN
+// LOGIN
 async function login() {
   const email = document.getElementById("email").value;
   const pass = document.getElementById("pass").value;
@@ -43,4 +43,11 @@ async function login() {
   } catch (error) {
     alert("Error: " + error.message);
   }
+}
+
+// LOGOUT
+function logout() {
+  auth.signOut();
+  localStorage.removeItem("rol");
+  window.location.href = "login.html";
 }
